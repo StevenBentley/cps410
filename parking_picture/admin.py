00b1e4d319af2a796201294344f171e0b0e8319b
Steven <stvnbentley@gmail.com>
2019-11-13 09:36:02 -0500

    backend changes

--- parking_picture/admin.py
+++ parking_picture/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ParkingPicture
 
 # Register your models here.
+class ParkingPictureAdmin(admin.ModelAdmin):
+    fields = ('picture', 'date')
+
+admin.site.register(ParkingPicture, ParkingPictureAdmin)
--- parking_picture/models.py
+++ parking_picture/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class ParkingPicture(models.Model):
-    picture = ImageField(upload_to=None, height_field=None, width_field=None,
-        max_length=100, **options)
-    captured_date = DateTimeField(auto_now=True, auto_now_add=True **options)
+    picture = models.ImageField(upload_to='media/', height_field=None, width_field=None,
+        max_length=100)
+    date = models.DateTimeField()
