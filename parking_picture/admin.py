21a009ec234898ae4916b6d416c17bc7f120dedb
bentl1sr <bentl1sr@cmich.edu>
2019-11-25 16:54:20 -0500

    fix admin

--- parking_picture/admin.py
+++ parking_picture/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import ParkingLot, ParkingPicture
+from .models import ParkingLot, ParkingPicture, ParkingLotSpots
 
 # Register your models here.
 class ParkingLotAdmin(admin.ModelAdmin):
     fields = ('lot_name',)
 
 class ParkingPictureAdmin(admin.ModelAdmin):
-    fields = ('picture', 'date')
+    fields = ('parking_lot', 'picture', )
+
+class ParkingLotSpotsAdmin(admin.ModelAdmin):
+    fields = ('parking_lot', 'data')
 
 admin.site.register(ParkingLot, ParkingLotAdmin)
 admin.site.register(ParkingPicture, ParkingPictureAdmin)
+admin.site.register(ParkingLotSpots, ParkingLotSpotsAdmin)
