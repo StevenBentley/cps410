1839715ec917f5b4215d11ca28858816a88dd1c7
bentl1sr <bentl1sr@cmich.edu>
2019-11-26 15:09:46 -0500

    rename data field to spot_status

--- parking_picture/admin.py
+++ parking_picture/admin.py
@@ -4,14 +4,12 @@ from .models import ParkingLot, ParkingPicture, ParkingLotSpots
 # Register your models here.
 class ParkingLotAdmin(admin.ModelAdmin):
     fields = ('lot_name',)
-    
+
 class ParkingPictureAdmin(admin.ModelAdmin):
     fields = ('parking_lot', 'picture', )
-    readonly_fields = ('parking_lot', )
 
 class ParkingLotSpotsAdmin(admin.ModelAdmin):
     fields = ('parking_lot', 'parking_spots', 'spot_status')
-    readonly_fields = ('parking_lot', 'parking_spots' )
 
 admin.site.register(ParkingLot, ParkingLotAdmin)
 admin.site.register(ParkingPicture, ParkingPictureAdmin)
