afeb7e18a18dd61876310ad6d149f624a63009b5
bentl1sr <bentl1sr@cmich.edu>
2019-11-27 16:56:41 -0500

    code abstract

---
+++ parking_picture/helpers.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+import hashlib, base64, hmac, datetime
+from .models import ParkingLot, ParkingPicture
+
+def handle_file_upload(lot, file):
+    now = datetime.datetime.now().strftime("%m-%d-%y_%H-%M-%S")
+    file_name = str(file)
+    file_name = lot + "_" + now + file_name[file_name.find('.'):]
+    with open("media/" + file_name, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+    store_picture(lot, file_name)
+
+def store_picture(lot, file):
+    try:
+        parking_lot = ParkingLot.objects.get(lot_name=lot)
+    except:
+        parking_lot = ParkingLot.objects.create(lot_name=lot)
+        parking_lot.save()
+    parking_picture = ParkingPicture.objects.create(parking_lot=parking_lot, picture=file)
+    parking_picture.save()
+
+def encryption_keys_match(request):
+    try:
+        public_key = settings.PI_SECRET_KEY[request.META["HTTP_PUBLICKEY"]]
+        signed_hmac = request.META["HTTP_HMAC"]
+        digest = hmac.new(public_key, msg=request.body, digestmod=hashlib.sha256)
+        msg = digest.hexdigest()
+        if hmac.compare_digest(msg, signed_hmac):
+            return True
+        else:
+            return False
+    except KeyError:
+        # Logging?
+        return False
--- parking_picture/views.py
+++ parking_picture/views.py
@@ -3,15 +3,12 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.template import loader
+from django.core.management import call_command
 
-import datetime
-import json
 import os, io, ast
-import hashlib, base64, hmac
-
 from .models import ParkingLot, ParkingPicture, ParkingLotSpots
 from .forms import ParkingPictureForm
-from django.core.management import call_command
+from .helpers import handle_file_upload, store_picture, encryption_keys_match
 
 def view_car_pics(request):
     template = loader.get_template('car_pics.html')
@@ -28,31 +25,13 @@ def parking_picture(request):
         return HttpResponse(403)
 
     if request.method == 'POST':
-        # for some reason the return response statements are messed up
-        # for example, when the keys dont match, "we here boooi" prints, but
-        # a 200 response is still returned?
-        try:
-            public_key = settings.PI_SECRET_KEY[request.META["HTTP_PUBLICKEY"]]
-            signed_hmac = request.META["HTTP_HMAC"]
-            digest = hmac.new(public_key,
-                msg=request.body, digestmod=hashlib.sha256)
-            msg = digest.hexdigest()
-            if not hmac.compare_digest(msg, signed_hmac):
-                return HttpResponse(401)
-
-        except KeyError:
-            return HttpResponse(400)
-
-        form = ParkingPictureForm(request.POST, request.FILES)
-        if form.is_valid():
-            handle_file_upload(request.POST['parking_lot'], request.FILES['picture'])
-            HttpResponseRedirect('/')
+        if encryption_keys_match(request):
+            form = ParkingPictureForm(request.POST, request.FILES)
+            if form.is_valid():
+                handle_file_upload(request.POST['parking_lot'], request.FILES['picture'])
+                return HttpResponse(200)
         else:
-            return HttpResponse(422)
-
-    return HttpResponse(200)
-        # Delete old file
-        # os.remove(old_file_location)
+            return HttpResponse(403)
 
 def parking_data(request, lot):
 
@@ -67,22 +46,3 @@ def parking_data(request, lot):
         space_status[space] = values[i]
 
     return JsonResponse(space_status)
-
-
-def handle_file_upload(lot, file):
-    now = datetime.datetime.now().strftime("%m-%d-%y_%H-%M-%S")
-    file_name = str(file)
-    file_name = lot + "_" + now + file_name[file_name.find('.'):]
-    with open("media/" + file_name, 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
-    store_picture(lot, file_name)
-
-def store_picture(lot, file):
-    try:
-        parking_lot = ParkingLot.objects.get(lot_name=lot)
-    except:
-        parking_lot = ParkingLot.objects.create(lot_name=lot)
-        parking_lot.save()
-    parking_picture = ParkingPicture.objects.create(parking_lot=parking_lot, picture=file)
-    parking_picture.save()
